5df5c917c4e44db237d2465108132685301a34c8
Sergey <serjjjoyl@gmail.com>
2022-07-01 05:15:08 +0300

    Outline of sizeSelecter

--- sfml/Menu/SizeSelecter.cs
+++ sfml/Menu/SizeSelecter.cs
@@ -19,37 +19,31 @@ namespace sfml
             xs = new Size();
             xs.Circle = new CircleShape();
             xs.Circle.Radius = 3;
-            xs.Circle.FillColor = Planets.ConstColor;
             xs.Circle.Position = new Vector2f(360, Sf.H - 30 - xs.Circle.Radius);
 
             s = new Size();
             s.Circle = new CircleShape();
             s.Circle.Radius = 5;
-            s.Circle.FillColor = Constants.GetColor(Colors.Orange);
-            s.Circle.Position = new Vector2f(370 + xs.Circle.Radius*2, Sf.H - 30 - s.Circle.Radius);
+            s.Circle.Position = new Vector2f(370 + xs.Circle.Radius * 2, Sf.H - 30 - s.Circle.Radius);
 
             m = new Size();
             m.Circle = new CircleShape();
             m.Circle.Radius = 10;
-            m.Circle.FillColor = Constants.GetColor(Colors.Yellow);
             m.Circle.Position = new Vector2f(385 + s.Circle.Radius * 2, Sf.H - 30 - m.Circle.Radius);
 
             l = new Size();
             l.Circle = new CircleShape();
             l.Circle.Radius = 15;
-            l.Circle.FillColor = Constants.GetColor(Colors.Green);
             l.Circle.Position = new Vector2f(405 + m.Circle.Radius * 2, Sf.H - 30 - l.Circle.Radius);
 
             xl = new Size();
             xl.Circle = new CircleShape();
             xl.Circle.Radius = 20;
-            xl.Circle.FillColor = Constants.GetColor(Colors.Blue);
             xl.Circle.Position = new Vector2f(435 + l.Circle.Radius * 2, Sf.H - 30 - xl.Circle.Radius);
 
             xxl = new Size();
             xxl.Circle = new CircleShape();
             xxl.Circle.Radius = 30;
-            xxl.Circle.FillColor = Constants.GetColor(Colors.Violet);
             xxl.Circle.Position = new Vector2f(475 + xl.Circle.Radius * 2, Sf.H - 30 - xxl.Circle.Radius);
 
             sizeList = new List<Size>() { xs, s, m, l, xl, xxl };
@@ -57,7 +51,9 @@ namespace sfml
             foreach (var sz in sizeList)
             {
                 sz.Circle.OutlineThickness = 1;
-                sz.Circle.FillColor = Planets.ConstColor;
+                sz.Circle.FillColor = Color.Transparent;
+                sz.Circle.OutlineColor = Color.White;
+                sz.Circle.OutlineThickness = 1;
             }
         }
         /// <summary>
@@ -69,14 +65,9 @@ namespace sfml
             var mouse = (Vector2f)Mouse.GetPosition(Sf.window);
             foreach (var sz in sizeList)
             {
-                sz.Circle.OutlineColor = Color.Transparent;
-            }
-            foreach (var sz in sizeList)
-            {
                 if (sz.Circle.GetGlobalBounds().Contains(mouse.X, mouse.Y))
                 {
-                    sz.Circle.OutlineThickness = 1;
-                    sz.Circle.OutlineColor = Color.White;
+                    sz.Circle.OutlineColor = Color.Red;
                     return sz;
                 }
             }
@@ -87,10 +78,11 @@ namespace sfml
             position = getpos;
             foreach (var sz in sizeList)
             {
+                sz.Circle.Position = new Vector2f(sz.Circle.Position.X, Sf.H - position - sz.Circle.Radius + 37);
                 if (sz.IsSelected)
-                    sz.Circle.Position = new Vector2f(sz.Circle.Position.X, Sf.H - position - sz.Circle.Radius + 35);
+                    sz.Circle.OutlineColor = Color.Red;
                 else
-                    sz.Circle.Position = new Vector2f(sz.Circle.Position.X, Sf.H - position - sz.Circle.Radius + 40);
+                    sz.Circle.OutlineColor = Color.White;
             }
             SelecterThickness();
             foreach (var sz in sizeList)
